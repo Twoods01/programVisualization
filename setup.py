0599c6e65c94780020b36bb719cef82ba3999768
twoods01 <twoods0129@gmail.com>
2015-04-27 15:12:30 -0700

    Setup update to include python version

--- setup.py
+++ setup.py
@@ -10,6 +10,20 @@ setup(
     author='twoods',
     author_email='twoods01@calpoly.edu',
     description='This program generates a visualization of an input java program',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Instructors :: Students',
+    'Topic :: Program Visualization',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2.7'
+    ],
     entry_points={
         'console_scripts': [
             'pjViz=pjViz:main',
